3fb560fcf1986e76966bc2938dc733e4b40e617f
Caroline Berg <caroline.berg@no.experis.com>
2024-02-28 08:50:09 +0100

    core requirements implemented

--- src/App.jsx
+++ src/App.jsx
@@ -5,40 +5,55 @@ import "./App.css"
 function App() {
   const [workouts, setWorkouts] = useState(initialWorkouts)
 
-  const addNewWorkout = () => {
+  const addNewWorkout = ()=>{
     const newWorkout = generateWorkout()
-    console.log("addNewWorkout:", newWorkout)
+    setWorkouts([...workouts, newWorkout])
+
   }
 
   const deleteWorkout = (workout) => {
-    console.log("deleteWorkout:", workout)
+    setWorkouts(workouts.filter(w => w !== workout))
   }
 
   const completeWorkout = (workout) => {
-    console.log("completeWorkout:", workout)
+    const updatedWorkouts = workouts.map((w) => {
+      if (w === workout) {
+        w.done =true;
+        return w;
+      } else {
+        return w;
+      }
+    });
+    setWorkouts(updatedWorkouts)
   }
-
   return (
     <div className="App">
       <h1>🏋️‍♀️Workout Generator</h1>
       <button onClick={addNewWorkout}>Add New Workout</button>
-      <ul>
-        {workouts.map((workout, index) => (
-          <li key={index}>
-            <p>
-              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
-            </p>
-            {!workout.done &&
-              <button onClick={e=>completeWorkout(workout)}>Done</button>}
-            {workout.done &&
-              <p>✅</p>}
-            <button onClick={e=>deleteWorkout(workout)}>Delete</button>
-          </li>
-        ))}
-      </ul>
-
+      <Workouts workouts={workouts} deleteWorkout={deleteWorkout} completeWorkout={completeWorkout}/>
     </div>
   )
 }
 
 export default App
+
+
+function Workouts(props){
+  return(
+    <>
+    <ul>
+        {props.workouts.map((workout, index) => (
+          <li key={index}>
+            <p>
+              {workout.sets}x sets of <strong>{workout.reps}x{workout.exercise}</strong> with {workout.rest} seconds rest
+            </p>
+            {!workout.done &&
+              <button onClick={e=>props.completeWorkout(workout)}>Done</button>}
+            {workout.done &&
+              <p>✅</p>}
+            <button onClick={e=>props.deleteWorkout(workout)}>Delete</button>
+          </li>
+        ))}
+    </ul></>
+  )
+}
